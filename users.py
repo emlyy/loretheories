b147e372e9c93364fb223c16c8ccd35f1f3cbcfc
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2023-03-27 23:00:41 +0300

    added register and login

---
+++ users.py
@@ -0,0 +1,29 @@
+from flask import session
+from werkzeug.security import check_password_hash, generate_password_hash
+from db import db
+
+def register(username, password):
+    hash_value = generate_password_hash(password)
+    try:
+        sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        db.session.commit()
+    except:
+        return False
+    return True
+
+def login(username, password):
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if user == None:
+        return False
+    if check_password_hash(user[1], password):
+        session["username"] = username
+        session["user_id"] = user[0]
+        return True
+    return False
+
+def logout():
+    del session["username"]
+    del session["user_id"]
