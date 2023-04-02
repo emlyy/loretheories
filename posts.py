c0bb547c3eb7db13550c2ceac36967506a51ca02
emlyy <92013457+emlyy@users.noreply.github.com>
2023-04-02 23:05:21 +0300

    Update posts.py

--- posts.py
+++ posts.py
@@ -10,10 +10,10 @@ def get_all_posts():
 
 def save_post(title, message):
     user_id = session["user_id"]
-    #try:
-    sql = text("INSERT INTO posts (user_id, title, message, posted_at) VALUES (:user_id, :title, :message, NOW())")
-    db.session.execute(sql, {"user_id":user_id, "title":title, "message":message})
-    db.session.commit()
-    #except:
-        #return False
+    try:
+        sql = text("INSERT INTO posts (user_id, title, message, posted_at) VALUES (:user_id, :title, :message, NOW())")
+        db.session.execute(sql, {"user_id":user_id, "title":title, "message":message})
+        db.session.commit()
+    except:
+        return False
     return True
