c94772868471995fbfd426e96ff456ac64310cf3
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2023-04-02 19:29:20 +0300

    add table posts

--- schema.sql
+++ schema.sql
@@ -1,1 +1,2 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE posts (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, title TEXT, message TEXT, posted_at TIMESTAMP);
