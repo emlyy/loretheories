db7fc6d45ff7fcca771965aa57f79a967fb03934
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2023-04-20 12:28:28 +0300

    add tables

--- schema.sql
+++ schema.sql
@@ -1,2 +1,5 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
 CREATE TABLE posts (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, title TEXT, message TEXT, posted_at TIMESTAMP);
+CREATE TABLE comments (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, message TEXT, post_id INTEGER REFERENCES posts, posted_at TIMESTAMP);
+CREATE TABLE tags (post_id INTEGER REFERENCES posts, tag TEXT);
+CREATE TABLE likes (post_id INTEGER REFERENCES posts, user_id INTEGER REFERENCES users);
