9cdd9dbfaf2bb80fae55bd764f1e036a9375dd21
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2023-05-04 20:36:27 +0300

    add sessions.py

---
+++ sessions.py
@@ -0,0 +1,15 @@
+from flask import session
+from sqlalchemy.sql import text
+from db import db
+
+def save_post(post_id):
+    session["post_id"] = post_id
+
+def save_category(category):
+    session["category"] = category
+
+def save_previous(previous):
+    session["previous"] = previous
+
+def save_search(word):
+    session["search"] = word
